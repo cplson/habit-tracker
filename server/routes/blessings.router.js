9ced1164eeb9434d47dd11f06b5f06d2d85774a2
James Jacobsen <edjcjacobsen@gmail.com>
2023-04-15 16:05:48 -0500

    added blessings router, added the route to the server, and added the GET route and tested it with postman

--- server/routes/blessings.router.js
+++ server/routes/blessings.router.js
@@ -5,27 +5,26 @@ const {
     rejectUnauthenticated,
   } = require('../modules/authentication-middleware');
 
+
 // GETS this users motivations
 router.get('/',rejectUnauthenticated, (req, res) => {
     // GET route code here
-  console.log('triggered motivations GET');
+  console.log('triggered blessings GET');
     const userId = req.user.id;
-    const queryText = `SELECT * FROM motivations
+    const queryText = `SELECT * FROM blessings
     WHERE user_id = $1;`;
 
     pool.query(queryText, [userId])
         .then(result => {
             res.send(result.rows);
         }).catch(err => {
-            console.log('There was an error getting motivations from the db');
+            console.log('There was an error getting blessings from the db');
             res.sendStatus(500);
         })
   
 });
 
-/**
- * POST route template
- */
+
 router.post('/', (req, res) => {
   // POST route code here
 });
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const motoRouter = require('./routes/motivations.router');
+const blessingsRouter = require('./routes/blessings.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -25,6 +26,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/motivations', motoRouter);
+app.use('/api/blessings', blessingsRouter);
 
 // Serve static files
 app.use(express.static('build'));
