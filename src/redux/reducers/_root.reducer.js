28c81275a6df61d8fb9102c97b1bafeaec710df7
James Jacobsen <edjcjacobsen@gmail.com>
2023-04-15 14:42:13 -0500

    added blessings reducer and combined it into rootReducer

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
 import motivations from './motivations.reducer';
+import blessings from './blessings.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -12,7 +13,8 @@ import motivations from './motivations.reducer';
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
-  motivations
+  motivations, // has an id, a user_id (fk), and the motivation
+  blessings // has an id, a user_id (fk), and the blessing
 });
 
 export default rootReducer;
