e20c8d2352b5a41b68247dba47fb1928742e6835
James Jacobsen <edjcjacobsen@gmail.com>
2023-04-16 16:30:06 -0500

    started setup of HabitsList by adding imports, added dispatch using useEffect, added habitsRouter skeleton and added route to server.js

--- src/components/HabitsList/HabitsList.jsx
+++ src/components/HabitsList/HabitsList.jsx
@@ -1,4 +1,16 @@
+import React, {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 function HabitsList(){
+    // get habits from the store
+    const habits = useSelector(store => store.habits);
+    // declare dispatch
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch({
+            type: 'FETCH_HABITS'
+        })
+    })
     return 'yay';
 }
 
