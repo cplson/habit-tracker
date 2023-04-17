a91eb41ff9409c683580ea8c5676cc89df083728
James Jacobsen <edjcjacobsen@gmail.com>
2023-04-16 20:37:23 -0500

    imported DeleteForeverIcon component from mui and added to each user habit

--- src/components/HabitsList/HabitsList.jsx
+++ src/components/HabitsList/HabitsList.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import HabitForm from '../HabitForm/HabitForm';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import Button from '@mui/material/Button';
 function HabitsList() {
     // get habits from the store
     const habits = useSelector(store => store.habits);
@@ -18,7 +20,7 @@ function HabitsList() {
             {
                 habits.length > 0 ?
                     <ul>
-                        {habits.map(habit => <li key={habit.id}>{habit.description}</li>)}
+                        {habits.map(habit => <li key={habit.id}>{habit.description} <Button><DeleteForeverIcon /></Button></li>)}
                     </ul>
                     :
                     <p>You do not yet have any habits</p>
