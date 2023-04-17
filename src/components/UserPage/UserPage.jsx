a8ffd938bae868bb1f2711d2ddd91ed7c69c55f2
James Jacobsen <edjcjacobsen@gmail.com>
2023-04-16 20:25:58 -0500

    added Button component import into UserPage

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import { useSelector } from 'react-redux';
 import Calendar from 'react-calendar';
+import Button from '@mui/material/Button';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
@@ -17,6 +18,7 @@ function UserPage() {
       <div>
         <Calendar />
       </div>
+      
       <LogOutButton className="btn" />
     </div>
   );
