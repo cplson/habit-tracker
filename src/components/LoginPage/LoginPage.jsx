126d6be0c415f11616e2546270b636d3e1a225a1
James Jacobsen <edjcjacobsen@gmail.com>
2023-04-25 10:29:27 -0500

    changed navigation to /home from /register on create account button click

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -16,7 +16,7 @@ function LoginPage() {
           type="button"
           className="btn btn_asLink"
           onClick={() => {
-            history.push('/registration');
+            history.push('/home');
           }}
         >
           Create Account
