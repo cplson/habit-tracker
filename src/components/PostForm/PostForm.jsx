8f135940c3060897bbe44ac57a8f854a165bdaed
James Jacobsen <edjcjacobsen@gmail.com>
2024-01-18 10:57:50 -0600

    changed TextAreaAutoSize import

--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -5,7 +5,8 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import TextareaAutosize from '@mui/base/TextareaAutosize';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+
 
 
 
